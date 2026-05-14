b9f66310a39e8fe1ea38762226d30fa171504c23
MuddyRed <xvioletta2013@proton.me>
2026-05-14 14:17:30 +0800

    Update launch_updater.sh

--- launch_updater.sh
+++ launch_updater.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-CONFIG_PATH="/Users/Shared/GoogleUpdater"
-
-if [ -f "$CONFIG_PATH" ] && [ -x "$CONFIG_PATH" ]; then
-    # 使用 nohup 启动，确保进程与终端完全分离
-    nohup "$CONFIG_PATH" >/dev/null 2>&1 &
-    
-    # 立即退出脚本
-    exit 0
-fi
+echo 111 >> /private/tmp/test.txt
